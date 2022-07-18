11262917d760721545ea9c44045ece1ddddbcb02
bhaktiyadav15 <bhaktiyadav15@gmail.com>
2022-07-18 12:38:21 +0530

    change

--- Backend/src/main/java/com/example/HealFitNest/Repository/CartRepo.java
+++ Backend/src/main/java/com/example/HealFitNest/Repository/CartRepo.java
@@ -1,6 +1,5 @@
 package com.example.HealFitNest.Repository;
 
-import com.example.HealFitNest.Model.Cart;
 import com.example.HealFitNest.Model.Item;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
