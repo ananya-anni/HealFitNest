9dba5be9c10f81dc7354df9aaab75f248713aa91
bhaktiyadav15 <bhaktiyadav15@gmail.com>
2022-07-21 22:19:38 +0530

    repo

--- Backend/src/main/java/com/example/HealFitNest/Repository/AddressRepo.java
+++ Backend/src/main/java/com/example/HealFitNest/Repository/AddressRepo.java
@@ -2,7 +2,9 @@ package com.example.HealFitNest.Repository;
 
 import com.example.HealFitNest.Model.Address;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepo extends MongoRepository<Address,String> {
 
 }
--- Backend/src/main/java/com/example/HealFitNest/Repository/CartRepo.java
+++ Backend/src/main/java/com/example/HealFitNest/Repository/CartRepo.java
@@ -1,10 +1,11 @@
 package com.example.HealFitNest.Repository;
 
 import com.example.HealFitNest.Model.Cart;
-import com.example.HealFitNest.Model.Item;
+//import com.example.HealFitNest.Model.Item;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CartRepo extends MongoRepository<Item,String> {
+import org.springframework.stereotype.Repository;
+@Repository
+public interface CartRepo extends MongoRepository<Cart,String> {
 
 }
